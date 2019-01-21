6a3d039329abed7bd120a0dc0fc4d606f00cca54
Lakhanskii Aleksei Andreevich <lakhanskii@ccfit.nsu.ru>
2019-01-21 23:23:06 +0700

    lab1.3(1,2)

---
+++ lab1.exp1,2.sh
@@ -0,0 +1,9 @@
+wget http://factorized.net/crusoe.txt
+find "*.txt"
+grep -io "friday" crusoe.txt | wc -w
+touch file1.txt
+echo "Hello world">file1.txt
+echo "Hello world second time">>file1.txt
+echo "Hello world">file1.txt
+ls
+find /home/ -name "*txt"
